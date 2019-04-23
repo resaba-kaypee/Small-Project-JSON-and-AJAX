ba1472d9ea5916f01ef7a5394067ab253e3471f9
kaypee <resaba.kaypee@gmail.com>
2019-04-24 03:24:14 +0700

    added some fucntionalities to processUsers

--- index.js
+++ index.js
@@ -2,21 +2,30 @@ window.onload = function () {
   let container = document.getElementById('animal-container');
   let btn = document.getElementById('btn');
     btn.addEventListener('click', async function(){
-      const response = await fetch('https://jsonplaceholder.typicode.com/users')
-      const users = await response.json();
+      let clicked = true;
+      let response = await fetch('https://jsonplaceholder.typicode.com/users')
+      let users = await response.json();
       let usersData = [];
+      
       for(let user of users){
         usersData.push(user)
       }
-       processUsers(usersData)
+      
+      if(clicked){
+        processUsers(usersData);
+        btn.classList.add("hide-me");
+      }
+       
     })
   
     function processUsers(users){
       var htmlString = '';
 
       for(let i = 0; i < users.length; i++){
-        htmlString += `<p>The user name ${users[i].name} email is ${users[i].email}.</p>`;
+        htmlString += `<p>${users[i].name} username is ${users[i].username} and lives in ${users[i].address.street} street ${users[i].address.city} city</p>`;
+        
       }
+
       container.insertAdjacentHTML('beforeend', htmlString)
     }
 
